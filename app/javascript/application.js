ae98be22a700c2df3a93b9417f8abfa12ffc9f40
karl <karl.aws123@gmail.com>
2024-10-25 21:04:08 +0300

    Fix erroneous esbuild configuration from rails default generator

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "./controllers"
+import { Application } from "@hotwired/stimulus"
+import ThemeController from "./controllers/theme_controller"
+
+const application = Application.start()
+application.register("theme", ThemeController)
