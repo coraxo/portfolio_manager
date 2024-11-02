139c6c23a8598849824b44cf0c9eb7097abed8cb
karl <karl.aws123@gmail.com>
2024-11-02 18:36:07 +0200

    Update cookie info, add about page with disclaimer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/cookie-consent", to: "cookies#consent"
   post "/cookie-consent", to: "cookies#consent"
   post "/reset-cookie-consent", to: "cookies#reset_consent"
+  get "/about", to: "welcome#about"
   resource :session
   resources :passwords, param: :token
   resources :users, except: [ :index, :destroy ]
